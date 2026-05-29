84fcd1e29c6c9b9139ddc61786e015dd200e5e63
Guo Zhang <zhangguo@quanttide.com>
2026-05-29 17:43:44 +0800

    chore: models → src/models，删除 services

--- packages/dart/lib/quanttide_finance.dart
+++ packages/dart/lib/quanttide_finance.dart
@@ -1,6 +1,5 @@
 /// 量潮金融领域模型
 library quanttide_finance;
 
-export 'models/journal.dart';
-export 'models/journal_entry.dart';
-export 'services/storage_backend.dart';
+export 'src/models/journal.dart';
+export 'src/models/journal_entry.dart';
